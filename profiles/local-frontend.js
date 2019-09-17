399b18f6bcfcd1038321751508b195d00c7192f0
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-09-17 15:59:53 +0200

    add first template

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,7 +1,7 @@
 /*global module*/
 
-const SECTION = 'insights';
-const APP_ID = 'starter';
+const SECTION = 'migrations';
+const APP_ID = 'rhamt';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
